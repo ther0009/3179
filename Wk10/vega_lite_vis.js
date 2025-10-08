d6cbcaf5e4373f344e4cd931428f7db7de070267
Tasha <ther0009@student.monash.edu>
2025-10-08 16:56:08 +1100

    adding second chart

--- Wk10/vega_lite_vis.js
+++ Wk10/vega_lite_vis.js
@@ -1,4 +1,10 @@
 var vg_1 = "gp_dot_density_map.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  // Access the Vega view instance as result.view
+}).catch(console.error);
+
+// NEW: second chart
+var vg_2 = "time_series.vg.json";
+vegaEmbed("#time_series", vg_2).then(function(result) {
+  // Access the Vega view instance as result.view
 }).catch(console.error);
