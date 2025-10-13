139eecba2a9e2fe21a8a2256cdae0ebfca934708
Tasha <ther0009@student.monash.edu>
2025-10-13 21:45:59 +1100

    fixed html issue

--- A2/vega_lite_vis.js
+++ A2/vega_lite_vis.js
@@ -14,6 +14,6 @@ vegaEmbed("#dumbell_plot", vg_3).then(function(result) {
 }).catch(console.error);
 
 var vg_4 = "bar_chart.vg.json";
-vegaEmbed("#bar_chart", vg_4).then(function(result) {
+vegaEmbed("#bar_chart_avg", vg_4).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
