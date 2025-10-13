f0e1781f2647ddb66f71619da45e1dfbe98367a7
Tasha <ther0009@student.monash.edu>
2025-10-13 21:40:25 +1100

    New vis

--- A2/vega_lite_vis.js
+++ A2/vega_lite_vis.js
@@ -12,3 +12,8 @@ var vg_3 = "dumbell_plot.vg.json";
 vegaEmbed("#dumbell_plot", vg_3).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
+
+var vg_4 = "bar_chart.vg.json";
+vegaEmbed("#bar_chart", vg_4).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
