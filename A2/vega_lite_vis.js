51f300e484b818c3826e75d3a2d2f18862b48c33
Tasha <ther0009@student.monash.edu>
2025-10-13 21:13:02 +1100

    dumbell plot

--- A2/vega_lite_vis.js
+++ A2/vega_lite_vis.js
@@ -3,8 +3,12 @@ vegaEmbed("#bar_chart", vg_1).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
 
-// NEW: second chart
 var vg_2 = "time_series.vg.json";
 vegaEmbed("#time_series", vg_2).then(function(result) {
+  // Access the Vega view instance as result.view
+}).catch(console.error);
+
+var vg_3 = "dumbell_plot.vg.json";
+vegaEmbed("#dumbell_plot", vg_3).then(function(result) {
   // Access the Vega view instance as result.view
 }).catch(console.error);
